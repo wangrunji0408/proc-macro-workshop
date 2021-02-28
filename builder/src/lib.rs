289bf5779319b6bf633f19bfb841f4f60a968c87
Runji Wang <wangrunji0408@163.com>
2021-02-28 20:37:28 +0800

    pass test builder-03

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -8,12 +8,19 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let name = input.ident;
     let builder_name = format_ident!("{}Builder", name);
     let builder_fields = gen_builder_fields(&input.data);
+    let builder_setters = gen_builder_setters(&input.data);
     let expanded = quote! {
         impl #name {
-            pub fn builder() {}
+            pub fn builder() -> #builder_name {
+                #builder_name::default()
+            }
         }
+        #[derive(Default)]
         pub struct #builder_name {
             #builder_fields
+        }
+        impl #builder_name {
+            #builder_setters
         }
     };
     expanded.into()
@@ -28,6 +35,29 @@ fn gen_builder_fields(data: &Data) -> TokenStream {
                     let ty = &f.ty;
                     quote_spanned! { f.span() =>
                         #name: Option<#ty>,
+                    }
+                });
+                quote! { #(#recurse)* }
+            }
+            Fields::Unnamed(fields) => panic!("Builder can not be derived at tuples"),
+            Fields::Unit => quote! {},
+        },
+        _ => panic!("Builder can only be derived at struct"),
+    }
+}
+
+fn gen_builder_setters(data: &Data) -> TokenStream {
+    match data {
+        Data::Struct(data) => match &data.fields {
+            Fields::Named(fields) => {
+                let recurse = fields.named.iter().map(|f| {
+                    let name = &f.ident;
+                    let ty = &f.ty;
+                    quote_spanned! { f.span() =>
+                        fn #name(&mut self, #name: #ty) -> &mut Self {
+                            self.#name = Some(#name);
+                            self
+                        }
                     }
                 });
                 quote! { #(#recurse)* }
