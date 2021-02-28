daf506a9394b709dbaaa8571f006343d3b71f05a
Runji Wang <wangrunji0408@163.com>
2021-02-28 18:46:45 +0800

    pass test builder-01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input = parse_macro_input!(input as DeriveInput);
+    let expanded = quote! {};
+    TokenStream::from(expanded)
 }
