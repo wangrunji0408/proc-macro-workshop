66908681150d5eea3c754d0beb3e5273a7d855eb
Runji Wang <wangrunji0408@163.com>
2021-02-28 20:33:12 +0800

    pass test builder-02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,40 @@
-use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote, quote_spanned};
+use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let expanded = quote! {};
-    TokenStream::from(expanded)
+    let name = input.ident;
+    let builder_name = format_ident!("{}Builder", name);
+    let builder_fields = gen_builder_fields(&input.data);
+    let expanded = quote! {
+        impl #name {
+            pub fn builder() {}
+        }
+        pub struct #builder_name {
+            #builder_fields
+        }
+    };
+    expanded.into()
+}
+
+fn gen_builder_fields(data: &Data) -> TokenStream {
+    match data {
+        Data::Struct(data) => match &data.fields {
+            Fields::Named(fields) => {
+                let recurse = fields.named.iter().map(|f| {
+                    let name = &f.ident;
+                    let ty = &f.ty;
+                    quote_spanned! { f.span() =>
+                        #name: Option<#ty>,
+                    }
+                });
+                quote! { #(#recurse)* }
+            }
+            Fields::Unnamed(fields) => panic!("Builder can not be derived at tuples"),
+            Fields::Unit => quote! {},
+        },
+        _ => panic!("Builder can only be derived at struct"),
+    }
 }
