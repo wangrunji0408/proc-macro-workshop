aa979f0ffecf83fec5f4ef66149afab30efd8410
Runji Wang <wangrunji0408@163.com>
2021-03-01 23:04:07 +0800

    pass test sorted-01

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,12 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Item};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
+    let item = parse_macro_input!(input as Item);
 
-    unimplemented!()
+    let output = quote! { #item };
+    output.into()
 }
