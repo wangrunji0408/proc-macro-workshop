9cd51385b76491d938a32e6aad76027d0640954a
Runji Wang <wangrunji0408@163.com>
2021-03-01 23:26:16 +0800

    pass test sorted-03

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -15,7 +15,22 @@ fn sorted2(args: TokenStream, input: Item) -> Result<TokenStream> {
     let _ = args;
 
     match &input {
-        Item::Enum(_item) => {}
+        Item::Enum(item) => {
+            for i in 1..item.variants.len() {
+                let last_ident = &item.variants[i - 1].ident;
+                let ident = &item.variants[i].ident;
+                if ident <= last_ident {
+                    let before_idx = (0..i)
+                        .rfind(|&j| item.variants[j].ident < *ident)
+                        .unwrap_or(0);
+                    let before_ident = &item.variants[before_idx].ident;
+                    return Err(Error::new(
+                        item.variants[i].span(),
+                        format!("{} should sort before {}", ident, before_ident),
+                    ));
+                }
+            }
+        }
         _ => return Err(Error::new(args.span(), "expected enum or match expression")),
     }
 
